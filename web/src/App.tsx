a9030959638edb727fc28c9fa1b82a7b70b3905c
Henrique Monteiro <henrique.monteiro.martins@hotmail.com>
2020-12-14 19:42:25 -0300

    Established Routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './assets/styles/global.css';
 
-import Landing from './pages/lending';
+import Routes from './router';
 
 function App() {
   return (
-    <Landing />
+    <Routes />
   );
 }
 
