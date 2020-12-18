dccde24501240c3fcfc91d874a6e155a7272d75b
Henrique Monteiro <henrique.monteiro.martins@hotmail.com>
2020-12-18 16:54:50 -0300

    created database

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,4 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/users', (request, response)=>{
+    return response.json({});
+});
+
 app.listen(3333);
