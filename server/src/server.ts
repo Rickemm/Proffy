cfe64b1e61afbc0ae131788098d9266adada797c
Henrique Monteiro <henrique.monteiro.martins@hotmail.com>
2020-12-18 17:50:53 -0300

    added classes functionality implemented

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.get('/users', (request, response)=>{
-    return response.json({});
+app.listen(3333, () => {
+    console.log('🚀️ Server started on port 3333!');
 });
-
-app.listen(3333);
