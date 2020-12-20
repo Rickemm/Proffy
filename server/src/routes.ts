61379dc67d842d37330fbd5d6efe68dcb395bd8e
Henrique Monteiro <henrique.monteiro.martins@hotmail.com>
2020-12-20 00:28:43 -0300

    config backend ended

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import ClassesConstroller from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
 const classesConstroller = new ClassesConstroller();
+const connectionsConstroller = new ConnectionsController();
 
 routes.get('/classes', classesConstroller.index);
 routes.post('/classes', classesConstroller.create);
+
+routes.get('/connections', connectionsConstroller.index)
+routes.post('/connections', connectionsConstroller.create)
 
 export default routes;
